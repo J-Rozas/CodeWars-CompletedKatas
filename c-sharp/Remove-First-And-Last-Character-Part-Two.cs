95376a6119c40abf7c3480a4380bf2928fc8357f
Emilio J. R. Lima (he/him) <67562547+J-Rozas@users.noreply.github.com>
2022-07-27 23:16:14 +0100

    First commit

---
+++ c-sharp/Remove-First-And-Last-Character-Part-Two.cs
@@ -0,0 +1,13 @@
+﻿namespace environmentForCodeWars
+{
+    using System;
+
+    public static class Kata
+    {
+        public static string? Array(string s)
+        {
+            var arrS = s.Split(",");
+            return arrS.Length < 3 ? null : string.Join(",", arrS[1..arrS.Length]);
+        }
+    }
+}
